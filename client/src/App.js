6511ebf1ef562ccab8cf4d6c858a60b940b1e1f6
phuchh27 <phuchh27@gmail.com>
2023-02-16 14:57:30 +0700

    feature/create-register

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./Pages/Home/Home";
+import { Login } from "./Pages/Login/Login";
+import { Register } from "./Pages/Register/Register";
+import { ForgotPW } from "./Pages/ForgotPassWord/ForgotPW";
+import { Profile } from "./Pages/Profile/Profile";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/forgotPW" element={<ForgotPW />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+      
+    </>
   );
 }
 
