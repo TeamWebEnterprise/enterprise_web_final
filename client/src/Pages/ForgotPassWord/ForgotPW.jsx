92eb55885a2d3c44c556c029ee6aa809b6856d53
quocld <quocle2208@gmail.com>
2023-02-19 12:15:18 +0700

    Merge branch 'feature/create-home-page-and-layout'

--- client/src/Pages/ForgotPassWord/ForgotPW.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ForgotPW = () => {
-  return (
-    <div>ForgotPW</div>
-  )
-}
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,19 @@
+import { Box,Stack } from "@mui/material";
+import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar/Siderbar";
+import Feed from "../../components/Feed/Feed"
 import React from 'react'
-import CreatePost from '../../components/CreatePost/CreatePost'
-export const Home = () => {
+export const Home = ({mode,setMode}) => {
   return (
-    <>
-      <CreatePost/>
-    </>
+    <Box setMode={setMode} mode={mode} bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar/>
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar setMode={setMode} mode={mode}/>
+          <Feed/>
+          <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>Rightbar</Box>
+         {/*  <Rightbar /> */}
+        </Stack>
+        {/* <Add /> */}
+      </Box>
   )
 }
