39ff290d477e0292042caf1469d5c902191423d3
quocld <quocle2208@gmail.com>
2023-02-25 22:07:46 +0700

    fix: update folder name

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
 import "./App.css";
 import { Route, Routes, Outlet } from "react-router-dom";
-import { Home } from "./Pages/Home/Home";
-import Login from "./Pages/Login/Login";
-import { Register } from "./Pages/Register/Register";
-import { ForgotPW } from "./Pages/ForgotPassWord/ForgotPW";
-import { Profile } from "./Pages/Profile/Profile";
-
+import { Home } from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import { Register } from "./pages/Register/Register";
+import { ForgotPW } from "./pages/ForgotPassWord/ForgotPW";
+import { Profile } from "./pages/Profile/Profile";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/forgotPW' element={<ForgotPW />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/forgotPW" element={<ForgotPW />}></Route>
 
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/profile' element={<Profile />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
       </Routes>
     </>
   );
