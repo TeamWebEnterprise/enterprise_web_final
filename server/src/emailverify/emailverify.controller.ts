ad00e8c873523d52b6e948b905dc772f4b2c463b
quocld <quocle2208@gmail.com>
2023-03-16 09:56:01 +0700

    fix: email verify

--- server/src/emailverify/emailverify.controller.ts
+++ server/src/emailverify/emailverify.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { EmailverifyService } from './emailverify.service';
 
 @Controller('emailverify')
 export class EmailverifyController {
   constructor(private readonly emailverifyService: EmailverifyService) {}
+
+  @Get('token/:token')
+  async emailVerifyToKen(@Param('token') token: string) {
+    const email = await this.emailverifyService.decodeConfirmationToken(token);
+    return this.emailverifyService.confirmVerify(email);
+  }
 }
