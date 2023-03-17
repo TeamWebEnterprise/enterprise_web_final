041ec0d855081e13fef40a6a482a0fd29f6c324f
quocld <quocle2208@gmail.com>
2023-03-17 09:56:03 +0700

    fix:mailtenplate

--- server/src/emailverify/emailverify.controller.ts
+++ server/src/emailverify/emailverify.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { EmailverifyService } from './emailverify.service';
 
 @Controller('emailverify')
 export class EmailverifyController {
   constructor(private readonly emailverifyService: EmailverifyService) {}
 
-  @Get('token/:token')
-  async emailVerifyToKen(@Param('token') token: string) {
+  @Get()
+  async emailVerifyToKen(@Query('token') token: string) {
     const email = await this.emailverifyService.decodeConfirmationToken(token);
     return this.emailverifyService.confirmVerify(email);
   }
